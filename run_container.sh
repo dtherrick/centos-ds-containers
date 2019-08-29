f430896943de1d0747b5129702f9685ef33eeaf4
Damian Herrick <damian.herrick@sas.com>
2019-08-28 20:51:20 -0400

    Update for NLP workshop

--- run_container.sh
+++ run_container.sh
@@ -3,5 +3,9 @@
 FOLDER=$1
 IMAGE=$2
 
-CONTAINER=`docker run -d --rm -p 8888:8888 -v $FOLDER:/home/ds/notebooks centos-ds/$IMAGE`
+CONTAINER=`docker run -d --rm -p 8888:8888 \
+           --name $IMAGE \
+           -v $FOLDER/notebooks/:/home/ds/notebooks \
+           -v $FOLDER/data/:/home/ds/datasets \
+           centos-ds/$IMAGE`
 echo $CONTAINER
