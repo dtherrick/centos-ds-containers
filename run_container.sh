3bfd20586c703e2540bab08e215471259e68c9f4
Damian Herrick <damian.herrick@sas.com>
2019-08-29 08:54:07 -0400

    fix directory mapping on run_container.sh

--- run_container.sh
+++ run_container.sh
@@ -6,7 +6,7 @@ IMAGE=$3
 
 CONTAINER=`docker run -d --rm -p 8888:8888 \
            --name $IMAGE \
-           -v $NOTEBOOK_FOLDER/notebooks/:/home/ds/notebooks \
-           -v $DATA_FOLDER/data/:/home/ds/datasets \
+           -v $NOTEBOOK_FOLDER:/home/ds/notebooks \
+           -v $DATA_FOLDER:/home/ds/datasets \
            centos-ds/$IMAGE`
 echo $CONTAINER
